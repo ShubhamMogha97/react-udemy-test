1f34cb5f41f59c3d42bc217aeaf8ebf00c372d08
Shubham Mogha <shubham.mogha97@gmail.com>
2026-05-25 16:52:34 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import './App.css'
+function App() {
+  return (
+    <div>
+      <h2>Learn React with Chai with code.com</h2>
+      <p>This is a Sub heading.</p>
+    </div>
+  )
+}
+export default App
